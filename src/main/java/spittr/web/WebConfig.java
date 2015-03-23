52e333cfe0654eeb9ecf42523e944b14e74e23cb
giolab <giolabdev@gmail.com>
2015-03-23 09:08:51 +0100

    Pom adapted to servlet container and secondary library. Response Negotiation code added in Web Config

--- src/main/java/spittr/web/WebConfig.java
+++ src/main/java/spittr/web/WebConfig.java
@@ -3,12 +3,19 @@ package spittr.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 @EnableWebMvc
@@ -23,6 +30,29 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     return resolver;
   }
   
+  @Bean
+  public ViewResolver cnViewResolver(ContentNegotiationManager cnm){
+      ContentNegotiatingViewResolver cnvr = new ContentNegotiatingViewResolver();
+      cnvr.setContentNegotiationManager(cnm);
+      return cnvr;
+  }
+  
+    // Overrided to define the default content type returned
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.TEXT_HTML);
+    }
+ 
+    @Bean
+    public ViewResolver beanNameViewResolver(){
+        return new BeanNameViewResolver();
+    }
+    
+    @Bean 
+    public View spittles(){
+        return new MappingJackson2JsonView();
+    }
+    
   @Override
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
